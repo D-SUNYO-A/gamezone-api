bebec70b3fd51909b6082d1cef1a5fd0da57b79f
D-SUNYO-A <sdanarson1@gmail.com>
2024-01-27 21:13:52 +0300

    version-10

--- utils/DbConnection.js
+++ utils/DbConnection.js
@@ -2,7 +2,7 @@
 
 import mongoose from "mongoose";
 
-export const connectToDatabase = () => {
+export const connectToDatabase = async () => {
   const uri = "mongodb+srv://sdanarson1:YF078se0zrRptXYn@cluster0.ebxzpgl.mongodb.net/blog?retryWrites=true&w=majority";
-  return mongoose.connect(uri);
+  return await mongoose.connect(uri);
 };
