0c32a2d2f5d1b4be0f75e6e1a2df7449928494ef
D-SUNYO-A <sdanarson1@gmail.com>
2024-01-27 18:18:31 +0300

    version-8

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import http from "http";
 import cluster from "cluster";
 import os from 'os';
-import { handleRoutes } from "./routes.js";
 import { connectToDatabase } from "./utils/DbConnection.js";
 import { corsMiddleware } from "./middleware/Cors.js";
+import { postController } from "./controllers/PostController.js";
+import { gameController } from "./controllers/GameController.js";
+import { responseUtil } from "./utils/ResponseUtil.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -28,7 +30,28 @@ if (cluster.isPrimary) {
         if (req.method === 'OPTIONS') {
             corsMiddleware.resWriteHead(req, res); 
         } else {
-            handleRoutes(req, res);
+            if(req.url.startsWith('/uploads/')) {
+                postController.getUploadsPost(req, res, import.meta.url);
+            } else if(req.url === '/api/posts' && req.method === 'POST') {
+                postController.createPost(req, res);
+            } else if(req.url === '/api/posts' && req.method === 'GET') {
+                postController.getPosts(req, res);  
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'GET') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.getPost(req, res, id);
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'PUT') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.updatePost(req, res, id);
+            } else if(req.url.match(/\/api\/post\/([0-9]+)/) && req.method === 'DELETE') {
+                const id = req.url.split('/')[3]; // api/post/1
+                postController.deletePost(req, res, id);
+            } else if(req.url === "/initialgames" && req.method === "GET") {
+                gameController.handleInitialGames(req, res);
+            } else if(req.url.startsWith("/games") && req.method === "GET") {
+                gameController.handleGames(req, res);
+            }  else {
+                responseUtil.responseNotFound(res);
+            }
         }
     });  
     
